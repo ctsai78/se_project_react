e3c2814008e132e29e30643835b011fcf0d5756e
Chang Min Tsai <changt@Changs-MacBook-Air.local>
2023-08-18 00:14:47 -0400

    Project 11

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -3,22 +3,27 @@ import "./AddItemModal.css";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 const AddItemModal = ({ handleCloseModal, onAddItem, isOpen }) => {
+  /* ------------------------------ Set handlers ------------------------------ */
   const [name, setName] = useState("");
   const handleNameChange = (e) => {
-    console.log(e.target.value);
     setName(e.target.value);
   };
 
   const [link, setUrl] = useState("");
   const handleUrlChange = (e) => {
-    console.log(e.target.value);
     setUrl(e.target.value);
+  };
+
+  const [weather, setWeather] = useState("");
+  const handleWeatherChange = (e) => {
+    setWeather(e.target.value);
   };
 
   const handleSubmit = (e) => {
     // e.preventDefault();
-    onAddItem({ name, link });
+    onAddItem({ name, link, weather });
   };
+  /* -------------------------------------------------------------------------- */
 
   return (
     <ModalWithForm
@@ -57,15 +62,33 @@ const AddItemModal = ({ handleCloseModal, onAddItem, isOpen }) => {
       <p className="modal_selection-title">Select the weather type:</p>
       <div className="modal_selection">
         <div className="modal_selection-item">
-          <input type="radio" id="hot" value="hot" name="weather" />
+          <input
+            type="radio"
+            id="hot"
+            value="hot"
+            name="weather"
+            onChange={handleWeatherChange}
+          />
           <label>Hot</label>
         </div>
         <div className="modal_selection-item">
-          <input type="radio" id="warm" value="warm" name="weather" />
+          <input
+            type="radio"
+            id="warm"
+            value="warm"
+            name="weather"
+            onChange={handleWeatherChange}
+          />
           <label>warm</label>
         </div>
         <div className="modal_selection-item">
-          <input type="radio" id="cold" value="cold" name="weather" />
+          <input
+            type="radio"
+            id="cold"
+            value="cold"
+            name="weather"
+            onChange={handleWeatherChange}
+          />
           <label>cold</label>
         </div>
       </div>
